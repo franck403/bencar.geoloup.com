9bcb81459dca255efb55be1424be9fce42bb998d
franck403 <79372051+franck403@users.noreply.github.com>
2023-01-17 10:55:45 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,4 +5,5 @@ function scriptIframe(){
 }
 var myFrame = $("#iframes").contents().find('body');
 var scropr = '<script src="https://bencar.geoloup.com/modify.js">' + "<" + "/" + "script>";
+console.log("script added")
 myFrame.html(scropr);
