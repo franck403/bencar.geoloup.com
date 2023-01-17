7808d3a15c3780bcd8a64107d9b1e3f50a400d5b
franck403 <79372051+franck403@users.noreply.github.com>
2023-01-17 09:28:28 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-import jquery from 'https://code.jquery.com/jquery-3.5.1.min.js';
 function scriptIframe(){
     	var myFrame = $("#iframes").contents().find('body');
       	var scropr = '<script src="https://bencar.geoloup.com/modify.js">' + "<" + "/" + "script>";
