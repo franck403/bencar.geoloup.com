e8d79457899dbb5ff537ce099f3366ee18693f4e
franck403 <79372051+franck403@users.noreply.github.com>
2023-01-17 10:43:52 -0500

    Update modify.js

--- modify.js
+++ modify.js
@@ -1,5 +1,5 @@
-document.getElementById('WIX_ADS').remove();
+const element = document.getElementById('WIX_ADS');
+element.remove();
 
 const site = document.getElementById('site-root');
-
 site.setAttribute("style", "margin-top:0px;");
